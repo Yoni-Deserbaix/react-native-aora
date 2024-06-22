e012fc269d120703d5b98ca077cf4bcf2ecd87a0
y2_dev <yonideserbaix@gmail.com>
2024-06-22 16:19:17 +0200

    feat(index): change route to home page

--- app/index.jsx
+++ app/index.jsx
@@ -6,8 +6,8 @@ export default function index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-3xl font-pblack">Aora!</Text>
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to profile
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
       </Link>
     </View>
   );
