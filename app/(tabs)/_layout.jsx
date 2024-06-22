2d4acef143c3ccf87c6ca23e3717a3c0eeb46358
y2_dev <yonideserbaix@gmail.com>
2024-06-22 16:44:06 +0200

    feat(tabs):create tabBar for home

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,10 +1,48 @@
+import { Tabs } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { icons } from "../../constants";
 
 export default function TabsLayout() {
-  return (
-    <View>
-      <Text>TabsLayout</Text>
+  const TabIcon = ({ icon, color, name, focused }) => (
+    <View className="flex justify-center items-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+      >
+        {name}
+      </Text>
     </View>
+  );
+
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
