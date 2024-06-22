a193a4eaf70486e39dacd96a17710f49296b2622
y2_dev <yonideserbaix@gmail.com>
2024-06-22 15:49:43 +0200

    style(app): add tailwind class

--- app/index.js
+++ app/index.js
@@ -1,23 +1,14 @@
 import { Link } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function index() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl">Aora!</Text>
       <Link href="/profile" style={{ color: "blue" }}>
         Go to profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
