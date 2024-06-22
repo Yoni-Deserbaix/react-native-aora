0c6db3767ef4d9bfa728af5c9c91a9313f5da19f
y2_dev <yonideserbaix@gmail.com>
2024-06-22 23:17:55 +0200

    feat(signIn):  add sign in button

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import CustomButton from "../../components/CustomButton";
 import FieldForm from "../../components/FieldForm";
 import { images } from "../../constants";
 
@@ -11,10 +12,14 @@ export default function signIn() {
     password: "",
   });
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = () => {};
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full h-full justify-center px-4 my-6">
+        <View className="w-full h-min-[83vh] justify-center px-4 my-6">
           <Image
             source={images.logo}
             className="w-[115px] h-[35px]"
@@ -35,6 +40,12 @@ export default function signIn() {
             value={form.password}
             handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles="mt-7"
+          />
+          <CustomButton
+            title="sign in"
+            handlePress={onSubmit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
           />
         </View>
       </ScrollView>
