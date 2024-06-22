345cd465ebc75f8faef64e395109075cdb42b64a
y2_dev <yonideserbaix@gmail.com>
2024-06-22 19:19:53 +0200

    fix(signIn): create and import FormField component

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Formfield from "../../components/Formfield";
 import { images } from "../../constants";
 
 export default function signIn() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full h-full justify-center items-center px-4 my-6">
+        <View className="w-full h-full justify-center px-4 my-6">
           <Image
             source={images.logo}
             className="w-[115px] h-[35px]"
@@ -16,6 +17,7 @@ export default function signIn() {
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Aora
           </Text>
+          <Formfield />
         </View>
       </ScrollView>
     </SafeAreaView>
