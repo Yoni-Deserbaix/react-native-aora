975b1e337dec3a8dc1734ea72e5f56e0d2bc44dd
y2_dev <yonideserbaix@gmail.com>
2024-06-22 19:44:32 +0200

    fix(signIn): create and import FormField component

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import FieldForm from "../../components/FieldForm";
 import { images } from "../../constants";
 
 export default function signIn() {
+  const [form, setform] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full h-full justify-center items-center px-4 my-6">
+        <View className="w-full h-full justify-center px-4 my-6">
           <Image
             source={images.logo}
             className="w-[115px] h-[35px]"
@@ -16,6 +23,19 @@ export default function signIn() {
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Aora
           </Text>
+          <FieldForm
+            title="Email"
+            value={form.email}
+            handleTextChange={(e) => setform({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-adress"
+          />
+          <FieldForm
+            title="Password"
+            value={form.password}
+            handleTextChange={(e) => setform({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
---
+++ components/FieldForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function FieldForm() {
+  return (
+    <View className="space-y-2">
+      <Text className="text-base text-gray-100 font-pmedium">Formfield</Text>
+    </View>
+  );
+}
