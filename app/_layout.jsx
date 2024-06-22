6a7667c336e382af54a7a6e0f44acdc9ef03bec9
y2_dev <yonideserbaix@gmail.com>
2024-06-22 23:02:56 +0200

    feat(layout): add remove the headerShown for pages

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -34,6 +34,9 @@ export default function RootLayout() {
     // Stack is equivalent to children in React
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
     </Stack>
   );
 }
