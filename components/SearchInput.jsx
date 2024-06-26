68b1f3e5a786694c1a296ac6a1c99ba40720cc02
y2_dev <yonideserbaix@gmail.com>
2024-06-26 12:30:02 +0200

    feat(searchInput):  create function to navigate to query page

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,19 +1,36 @@
+import { router, usePathname } from "expo-router";
 import React, { useState } from "react";
-import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Image, TextInput, TouchableOpacity, View } from "react-native";
 import { icons } from "../constants";
 
 export default function SearchInput({ value, handleChangeText }) {
-  const [showPassword, setShowPassword] = useState(false);
+  const pathname = usePathname(); // get the current path
+  const [query, setQuery] = useState("");
   return (
     <View className="border-2 flex-row border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center space-x-4">
       <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
-        value={value}
+        value={query}
         placeholder="Search for a video topic..."
-        placeholderTextColor="#7b7b8b"
-        onChangeText={handleChangeText}
+        placeholderTextColor="#cdcde0"
+        onChangeText={(e) => setQuery(e)}
       ></TextInput>
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert(
+              "Missing query",
+              "Please input something to search result across database"
+            );
+          }
+
+          if (pathname.startsWith("/search")) {
+            router.setParams({ query }); // update query pathname if the user already on the search page
+          } else {
+            router.push(`/search/${query}`); // navigate to search query
+          }
+        }}
+      >
         <Image
           source={icons.search}
           className="w-5 h-5"
