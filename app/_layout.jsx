8338e0d9a4f1d481f41fb746e6f4c1541cefe1f5
y2_dev <yonideserbaix@gmail.com>
2024-06-26 13:41:13 +0200

    fix(layout): decomment stack screen for search page

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,7 @@
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import React, { useEffect } from "react";
-import GlobalProvider from "../context/GlobalProvider"
+import GlobalProvider from "../context/GlobalProvider";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -37,7 +37,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
       </Stack>
     </GlobalProvider>
   );
