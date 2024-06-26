5e81ede9abe11985c4f77f2d130a056221456dd0
y2_dev <yonideserbaix@gmail.com>
2024-06-26 23:30:28 +0200

    feat(createVidep): add input video

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,7 +1,10 @@
+import { ResizeMode, Video } from "expo-av";
 import React, { useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Image } from "react-native-animatable";
 import { SafeAreaView } from "react-native-safe-area-context";
 import FieldForm from "../../components/FieldForm";
+import { icons } from "../../constants";
 
 export default function Create() {
   const [uploading, setUploading] = useState(false);
@@ -22,7 +25,32 @@ export default function Create() {
           handleChangeText={(e) => setForm({ ...form, title: e })}
           otherStyles="mt-10"
         />
-        <View className="mt-10 "></View>
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Upload video
+          </Text>
+          <TouchableOpacity>
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                className="w-full h-64 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
+                <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    className="w-1/2 h-1/2"
+                  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
