3c952ff91a5a951ba9a6076eeac2f9e640d3972a
y2_dev <yonideserbaix@gmail.com>
2024-06-26 23:13:00 +0200

    feat(profile): add user info

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FlatList, Image, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import EmptyState from "../../components/EmptyState";
+import InfoBox from "../../components/InfoBox";
 import VideoCard from "../../components/VideoCard";
 import { icons } from "../../constants";
 import { useGlobalContext } from "../../context/GlobalProvider";
@@ -47,8 +48,33 @@ export default function Profile() {
                 source={icons.logout}
                 className="w-6 h-6"
                 resizeMode="contain"
-              ></Image>
+              />
             </TouchableOpacity>
+            <View className="h-16 w-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-3"
+              titleStyles="text-lg"
+            />
+            <View className="mt-3 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-5"
+                titleStyles="text-xl"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
+            </View>
           </View>
         )}
         // Conditional rendering if there are no videos
---
+++ components/InfoBox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function InfoBox({
+  title,
+  subtitle,
+  containerStyles,
+  titleStyles,
+}) {
+  return (
+    <View className={containerStyles}>
+      <Text className={`text-white text-center font-psemibold ${titleStyles}`}>
+        {title}
+      </Text>
+      <Text className="text-sm text-gray-100 text-center font-pregular">
+        {subtitle}
+      </Text>
+    </View>
+  );
+}
