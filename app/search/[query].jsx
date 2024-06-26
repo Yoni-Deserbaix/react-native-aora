449528addca0b0b6a2cb5d9dce305746af3da3b3
y2_dev <yonideserbaix@gmail.com>
2024-06-26 12:21:22 +0200

    feat(query):  create useLocalSearchParams hook

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,10 +1,13 @@
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function query() {
+  const { query } = useLocalSearchParams(); // Hook to get params // like useSearchParams in next.js
   return (
-    <View>
-      <Text>query</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <Text className="text-3xl text-white">{query}</Text>
+    </SafeAreaView>
   );
 }
