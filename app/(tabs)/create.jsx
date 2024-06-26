9b471c80e588822fe23948d4bb06f737f9e010d1
y2_dev <yonideserbaix@gmail.com>
2024-06-26 23:13:41 +0200

    feat(createVidep): add title input

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FieldForm from "../../components/FieldForm";
 
-export default function create() {
+export default function Create() {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
   return (
-    <View>
-      <Text>create</Text>
-    </View>
-  )
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white">Upload Video</Text>
+        <FieldForm
+          title="Video title"
+          value={form.title}
+          placeholder="Get your video a catch title..."
+          handleChangeText={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+        <View className="mt-10 "></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
