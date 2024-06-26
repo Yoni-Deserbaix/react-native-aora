2b082f49862cb31ab2e4c26c99c232837d9e0bd5
y2_dev <yonideserbaix@gmail.com>
2024-06-26 13:42:52 +0200

    feat(query): fetch videos

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,56 @@
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect } from "react";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../../components/EmptyState";
+import SearchInput from "../../components/SearchInput";
+import VideoCard from "../../components/VideoCard";
+import { searchPosts } from "../../lib/appwrite";
+import useAppWrite from "../../lib/useAppWrite";
 
-export default function query() {
-  const { query } = useLocalSearchParams(); // Hook to get params // like useSearchParams in next.js
+export default function Search() {
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppWrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
+  // console.log(query, posts);
+
   return (
-    <SafeAreaView className="bg-primary h-full">
-      <Text className="text-3xl text-white">{query}</Text>
+    <SafeAreaView className="bg-primary  h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id} // FlatList is like map in JS
+        renderItem={({ item }) => (
+          <VideoCard
+            video={item.video}
+            title={item.title}
+            thumbnail={item.thumbnail}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Results
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">{query}</Text>
+            <View className="mt-6 mb-8 ">
+              <SearchInput initialQuery={query} refetch={refetch} />
+            </View>
+          </View>
+        )}
+        // Conditional rendering if there are no videos
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
     </SafeAreaView>
   );
 }
