1dac0289bed72242588801f11d0dd091a6e5b523
y2_dev <yonideserbaix@gmail.com>
2024-06-26 15:27:32 +0200

    fix(profile): fetch and display posts from user

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,52 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../../components/EmptyState";
+import VideoCard from "../../components/VideoCard";
+import { icons } from "../../constants";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { getUserPosts } from "../../lib/appwrite";
+import useAppWrite from "../../lib/useAppWrite";
 
-const Profile = () => {
+export default function Profile() {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts } = useAppWrite(() => getUserPosts(user.$id));
+
+  // console.log(query, posts);
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary  h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id} // FlatList is like map in JS
+        renderItem={({ item }) => (
+          <VideoCard
+            video={item.video}
+            title={item.title}
+            thumbnail={item.thumbnail}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity className="w-full items-end mb-10">
+              <Image
+                source={icons.logout}
+                className="w-6 h-6"
+                resizeMode="contain"
+              ></Image>
+            </TouchableOpacity>
+          </View>
+        )}
+        // Conditional rendering if there are no videos
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
   );
-};
-
-export default Profile;
-
-const styles = StyleSheet.create({});
+}
