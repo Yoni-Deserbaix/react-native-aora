a0b58b5e8dda17121d869343c23cfd1ae038e960
y2_dev <yonideserbaix@gmail.com>
2024-06-27 15:24:58 +0200

    feat(createVidep): add thumbnail video input

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -51,6 +51,31 @@ export default function Create() {
             )}
           </TouchableOpacity>
         </View>
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Thumbnail video
+          </Text>
+          <TouchableOpacity>
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode="cover"
+                className="w-full h-64 rounded-2xl"
+              />
+            ) : (
+              <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2">
+                <Image
+                  source={icons.upload}
+                  resizeMode="contain"
+                  className="w-5 h-5"
+                />
+                <Text className="text-sm text-gray-100 font-pmedium">
+                  Choose a file
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
