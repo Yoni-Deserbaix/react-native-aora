81e4d8f97767ad6d924e150cfe0b75afb6e2cac9
y2_dev <yonideserbaix@gmail.com>
2024-07-01 16:15:24 +0200

    fix(videCard): add dropdown menu

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -2,7 +2,6 @@ import { ResizeMode, Video } from "expo-av";
 import React, { useState } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { icons } from "../constants";
-import DropdownMenu from "./DropdownMenu";
 
 export default function VideoCard({
   title,
@@ -13,6 +12,15 @@ export default function VideoCard({
   creator,
 }) {
   const [play, setPlay] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleSelect = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const handleDelete = () => {
+    console.log("Delete pressed");
+  };
 
   return (
     <View className="flex flex-col items-center px-4 mb-14">
@@ -43,8 +51,43 @@ export default function VideoCard({
         </View>
 
         <View className="pt-2">
-          {/* Ensure high z-index */}
-          <DropdownMenu />
+          <View className="relative">
+            <TouchableOpacity onPress={handleSelect}>
+              <Image
+                source={icons.menu}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+            {isVisible && (
+              <View className="border border-[#232533] px-4 rounded-xl absolute top-10 right-0 h-[85px] w-[130px] z-50 gap-2 bg-[#1E1E2D] justify-start items-center">
+                <TouchableOpacity
+                  onPress={() => console.log("Save pressed")}
+                  className="flex-row items-center w-full"
+                >
+                  <Image
+                    source={icons.bookmark}
+                    className="w-4 h-4 mr-2"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-lg text-white font-pmedium">Save</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => handleDelete()}
+                  className="flex-row items-center w-full"
+                >
+                  <Image
+                    source={icons.trash}
+                    className="w-[20px] h-[20px] -ml-0.5 mr-1.5"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-lg text-white font-pmedium">
+                    Delete
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
         </View>
       </View>
 
